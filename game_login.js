d64324b0db1100f560d7d08f8824c5184b82b1ad
JoaquinP15 <94157179+JoaquinP15@users.noreply.github.com>
2022-03-07 15:28:47 +0800

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,10 @@
+function adduser()
+{
+    player1 = document.getElementById("player1_name_input").value;
+    player2 = document.getElementById("player2_name_input").value;
+
+    localStorage.setItem("player1", player1);
+    localStorage.setItem("player2", player2);
+
+    window.location.replace("game_page.html");
+}
